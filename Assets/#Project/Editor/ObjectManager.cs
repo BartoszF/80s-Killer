0c7c332e5f596f3fe32e21a2314be94facc7759d
BartoszF <felis.bartosz@gmail.com>
2017-11-02 22:05:27 +0100

    Some pixels. And Unity Tilemap

---
+++ Assets/#Project/Editor/ObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectManager : EditorWindow {
+
+    [MenuItem("80s Killer/Object Manager")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(ObjectManager));
+    }
+
+    void OnGUI()
+    {
+        // The actual window code goes here
+    }
+}
